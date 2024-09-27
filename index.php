93d8d0f2febe6ea172fbaa423a55edb20a2a8523
Josh <joshua.holtz@outlook.com>
2024-09-27 15:35:08 +0100

    Dynamic PHP Nav Bar

---
+++ index.php
@@ -0,0 +1,28 @@
+<!-- 
+Within this file if it was to be a landing page or a random page in the website all we need to do
+to include the nav bar would be to call the file using 
+php include("linkToFile.php"); 
+
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<!-- PHP NAVBAR LINK -->
+<?php include("navigation-bar.php"); ?> 
+
+<!-- MAIN CONTENT -->
+<main>
+<h1 class="text-center text-2xl p-10 font-bold">Welcome To My Dynamic Navigation Bar</h1>
+</main>
+
+  
+</body>
+</html>
+
+
---
+++ navigation-bar.php
@@ -0,0 +1,39 @@
+<!-- 
+NAVBAR 
+When making a website you might find that you have 500 pages and they all have a navigation bar
+if you need to remove or add a page you will have to update all 500 pages so the nav bar is correct.
+
+We can use HTML and CSS/JS to create this nav bar and then paste it in using PHP
+and call it where we ant it. 
+
+Within this file you will add your HTML and you can link your CSS like normal. With:
+<link rel="stylesheet" href="filename.css"> 
+Then within the files where you want your navigation bar you can reference it with something like this:
+
+<php include("navigation-bar.php"); ?> //Include the ?
+
+This means if I was to make a change to the navbar in the navigation-bar.php file it will update it all!
+
+-->
+
+<!-- navigation-bar.php -->
+
+<link rel="stylesheet" href="navbar.css"> 
+
+<nav>
+    <ul>
+        <li><a href="#">Home</a></li>
+         <div class="dropdown">
+            <button class="dropbtn">Services <i class="fa-solid fa-chevron-down"></i></button>
+            <div class="dropdown-content">
+                <a href="#">Service 1</a>
+                <a href="#">Service 2</a>
+                <a href="#">Service 3</a>
+            </div>
+         </div>
+        <li><a href="#">About</a></li>
+        <div class="floatright">
+            <li><a href="#">Contact</a></li>
+        </div>
+    </ul>
+</nav>
